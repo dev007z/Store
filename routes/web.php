86286e6110c0f8c563ca8bf80f6652add71fe3b4
Junaidisgood <62109954+Junaidisgood@users.noreply.github.com>
2020-07-12 16:54:30 +0100

    set usersController@index to default page

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', 'UsersController@index')->name('Index');
 
 Auth::routes();
 
