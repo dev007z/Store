ad8ddd3e80c4a1a7a87873395c45b8119d2f0539
Junaidisgood <62109954+Junaidisgood@users.noreply.github.com>
2020-07-14 12:33:56 +0100

    change jquery file name & test if it's working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,6 +78,7 @@
                 <div class="row">
                     <div class="col-lg-4">
                         <form class="form-horizontal" method="POST" action="">
+                            {{csrf_field()}}
                             <div class="form-group row">
                                 <div class="col-8">
                                     <input type="text" name="searchonProduct" class="form-control" placeholder="Search Product">
@@ -115,3 +116,20 @@
     </div>
 </body>
 </html>
+
+{{-- JQuery link --}}
+<script src="{{ asset('js/jquery.js') }}"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#state').keyup(function(){
+            alert("Hello");
+        });
+    });
+</script>
+
+{{-- JQuery CDN 
+    <script
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
+    --}}
