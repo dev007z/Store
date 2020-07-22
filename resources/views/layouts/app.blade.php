0cab37259b91342beb77539e9001812da5fba87e
Junaidisgood <62109954+Junaidisgood@users.noreply.github.com>
2020-07-22 21:24:41 +0100

    show cities syled and modified

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,7 @@
                             </button> --}}
                             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                                 <!-- Left Side Of Navbar -->
+                            </div>
                         </ul>
 
                         <!-- Right Side Of Navbar -->
