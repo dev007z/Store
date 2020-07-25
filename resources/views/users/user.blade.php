8cb3fdbe948668c982d93af82f2f23b42bebda93
Junaidisgood <62109954+Junaidisgood@users.noreply.github.com>
2020-07-25 23:13:34 +0100

    hover effect on side categories menu

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -14,7 +14,7 @@
                         @if(isset($categories))
                             @if(count($categories)>0)
                                 @foreach ($categories as $category)
-                                    <li class="list-group-item-action list-group-item">
+                                    <li class="list-group-item-action list-group-item hover-danger">
                                         <a class="nav-link text-danger" href="{{ url('/products/'.preg_replace('/\s+/', '', $category->main_category).'/'.$category->id) }}">{!!html_entity_decode($category->icons)!!}{{$category->main_category}}</a>
                                     </li>
                                 @endforeach
